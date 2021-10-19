484b1500e45151fa679c65084678df069ded10c8
Kenneth <kennbyee25@gmail.com>
2021-10-19 15:38:00 -0500

    dismantle monolith begin retiring yeelight basic rms printing (for testing led strips)

--- chroma.py
+++ chroma.py
@@ -1,12 +1,11 @@
 import pyaudio
-import audioop
-import numpy as np
 from config import *
 from display import Display
 
 FORMAT = pyaudio.paFloat32
+
 p = pyaudio.PyAudio()
-d = Display("hsv")
+d = Display()
 
 # Initialize audio stream with config
 stream = pyaudio.Stream(p,
@@ -14,46 +13,12 @@ stream = pyaudio.Stream(p,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
-                        input_device_index=DEVICE)
-
-# Array of frequency values associated with sample indices
-fftfreq = np.fft.rfftfreq(CHUNK, d=1 / RATE)[1:]
-# Array of MIDI values associated with sample indices
-pitch_class = 12 * np.log2(fftfreq / 440)
-# Array of weights [0, 1] relative to grey noise (approximately)
-grey = 1 - np.power(2 * np.abs(0.5 - np.power(np.linspace(0, 1, CHUNK // 2), 0.3)), 2)
-# Array of weights [-1, 1] to determine color saturation
-chroma = np.cos(np.pi * pitch_class) * grey
-# Array of hues [0, 1]
-H = ((np.sign(chroma) * 3 - pitch_class + 8) % 12) / 12
-# Array of respective theta for trigonometric calculations
-Thetas = H * (2 * np.pi)
+                        input_device_index=DEVICE,
+                        )
 
 while stream.is_active():
-    i = np.random.randint(0, CHUNK // 2)
     data = stream.read(CHUNK, False)
-    rms = audioop.rms(data, 2)
-    data = np.frombuffer(data, np.float32)
-    fft = np.abs(np.fft.rfft(data))[1:]
-    # Volume * weight = value
-    V = np.abs((chroma) * (fft))
-    # place into polar coordinates
-    V_pol = (V * np.cos(Thetas), V * np.sin(Thetas))
-    # sum of vectors
-    XY = np.sum(V_pol, 1)
-    # hue is a function of the angle of the sum vector
-    hue = int(360 * ((np.arctan2(XY[1], XY[0]) / (2 * np.pi)) % 1))
-    # magnitude of sum vector
-    norm = np.linalg.norm(XY)
-    # sum of vector magnitudes
-    sum = np.sum(np.abs(V))
-    # random walk (colorless noise) is sqrt(N)
-    sat = int(100 * (norm / np.sqrt(sum) > 1))
-    sat = np.clip(sat, 1, 100)
-    # dynamic order polynomial on 75-100 dB for value
-    val = int(100 * np.power((rms - SENSITIVITY) / (18000-SENSITIVITY), 18))
-    val = np.clip(val, 1, 100)
-    d.display(hue, sat, val)
+    d.process(data)
 
 
 stream.stop_stream()
--- display.py
+++ display.py
@@ -1,14 +1,31 @@
 import yeelight
+import audioop
+# import numpy as np
+from config import *
 
 # Display handles the visual effects of
 class Display:
     def __init__(self, mode="hsv"):
         self.mode = mode
-        self.modes = {
+        self.bulb_modes = {
             "rgb": self.displayRGB,
             "hsv": self.displayHSV,
         }
-        self.findDevices()
+        if self.bulb_modes in ["rgb", "hsv"]:
+            self.findDevices()
+
+        # # Array of frequency values associated with sample indices
+        # self.fftfreq = np.fft.rfftfreq(CHUNK, d=1 / RATE)[1:]
+        # # Array of MIDI values associated with sample indices
+        # self.pitch_class = 12 * np.log2(self.fftfreq / 440)
+        # # Array of weights [0, 1] relative to grey noise (approximately)
+        # self.grey = 1 - np.power(2 * np.abs(0.5 - np.power(np.linspace(0, 1, CHUNK // 2), 0.3)), 2)
+        # # Array of weights [-1, 1] to determine color saturation
+        # self.chroma = np.cos(np.pi * self.pitch_class) * self.grey
+        # # Array of hues [0, 1]
+        # self.H = ((np.sign(self.chroma) * 3 - self.pitch_class + 8) % 12) / 12
+        # # Array of respective theta for trigonometric calculations
+        # self.Thetas = self.H * (2 * np.pi)
 
     def findDevices(self):
         ips = [bulb["ip"] for bulb in yeelight.discover_bulbs()]
@@ -16,11 +33,46 @@ class Display:
         for bulb in self.bulbs:
             bulb.start_music()
 
-    def display(self, x1, x2, x3):
-        self.modes[self.mode](x1, x2, x3)
+    def display_bulb(self, x1, x2, x3):
+        self.bulb_modes[self.mode](x1, x2, x3)
 
     def displayRGB(self, r, g, b):
         self.bulbs[0].set_rgb(r, g, b)
 
     def displayHSV(self, h, s, v):
         self.bulbs[0].set_hsv(h, s, v)
+
+    def process_bulb_HSV(self, data):
+        rms = audioop.rms(data, 2)
+        # data = np.frombuffer(data, np.float32)
+        # fft = np.abs(np.fft.rfft(data))[1:]
+        # # Volume * weight = value
+        # V = np.abs((self.chroma) * (fft))
+        # # place into polar coordinates
+        # V_pol = (V * np.cos(self.Thetas), V * np.sin(self.Thetas))
+        # # sum of vectors
+        # XY = np.sum(V_pol, 1)
+        # # hue is a function of the angle of the sum vector
+        # hue = int(360 * ((np.arctan2(XY[1], XY[0]) / (2 * np.pi)) % 1))
+        # # magnitude of sum vector
+        # norm = np.linalg.norm(XY)
+        # # sum of vector magnitudes
+        # sum = np.sum(np.abs(V))
+        # # random walk (colorless noise) is sqrt(N)
+        # sat = int(100 * (norm / np.sqrt(sum) > 1))
+        # sat = np.clip(sat, 1, 100)
+        # # dynamic order polynomial on 75-100 dB for value
+        # val = int(100 * np.power((rms - SENSITIVITY) / (18000 - SENSITIVITY), 18))
+        # val = np.clip(val, 1, 100)
+        self.display_bulb(hue, sat, val)
+
+    def process_led_strip(self, data):
+        rms = audioop.rms(data, 2)
+        print(rms)
+
+    def process(self, data):
+        switch = {
+            "hsv": self.process_bulb_HSV,
+            "smd5050": self.process_led_strip,
+        }
+        switch["smd5050"](data)
